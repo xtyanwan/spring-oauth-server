52504efe4cb4d19bfc12f22eb9172343473b6201
lishengzhao <shengzhao@honyee.cc>
2013-11-19 17:41:28 +0800

    Change comment

--- src/main/java/cc/honyee/web/oauth/OauthUserApprovalHandler.java
+++ src/main/java/cc/honyee/web/oauth/OauthUserApprovalHandler.java
@@ -1,17 +1,13 @@
 /*
- * Copyright 2002-2011 the original author or authors.
+ * Copyright (c) 2013 Honyee Industry Group Co., Ltd
+ * www.honyee.biz
+ * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software is the confidential and proprietary information of
+ * Honyee Industry Group Co., Ltd ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with Honyee Industry Group Co., Ltd.
  */
 
 package cc.honyee.web.oauth;
@@ -33,13 +29,6 @@ public class OauthUserApprovalHandler extends TokenServicesUserApprovalHandler {
     }
 
 
-    /**
-     * Allows automatic approval for a white list of clients in the implicit grant case.
-     *
-     * @param authorizationRequest The authorization request.
-     * @param userAuthentication   the current user authentication
-     * @return Whether the specified request has been approved by the current user.
-     */
     public boolean isApproved(AuthorizationRequest authorizationRequest, Authentication userAuthentication) {
         if (super.isApproved(authorizationRequest, userAuthentication)) {
             return true;
