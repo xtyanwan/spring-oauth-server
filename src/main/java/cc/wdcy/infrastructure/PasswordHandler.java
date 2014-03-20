3f6473a4156ed2b838e80c350aeda0e0bed9f63b
lishengzhao <shengzhao@honyee.cc>
2014-03-20 18:27:42 +0800

    Change to wdcy

--- src/main/java/cc/wdcy/infrastructure/PasswordHandler.java
+++ src/main/java/cc/wdcy/infrastructure/PasswordHandler.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2013 Honyee Industry Group Co., Ltd
- * www.honyee.biz
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Honyee Industry Group Co., Ltd ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with Honyee Industry Group Co., Ltd.
- */
 package cc.wdcy.infrastructure;
 
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
