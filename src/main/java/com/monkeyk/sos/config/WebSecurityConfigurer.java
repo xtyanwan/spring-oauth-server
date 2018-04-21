df96559693f06b0b86ede86da7dd846c05e365f0
monkeyk7 <zhao@idsmanager.com>
2018-04-21 12:22:01 +0800

    Update config

--- src/main/java/com/monkeyk/sos/config/WebSecurityConfigurer.java
+++ src/main/java/com/monkeyk/sos/config/WebSecurityConfigurer.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * 2016/4/3
