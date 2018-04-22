3c644555230ca0f1d0aa7c687ab32691998b19ef
monkeyk7 <zhao@idsmanager.com>
2018-04-22 14:12:49 +0800

    Unit test update

--- src/test/java/com/monkeyk/sos/ContextTest.java
+++ src/test/java/com/monkeyk/sos/ContextTest.java
@@ -1,17 +1,25 @@
 package com.monkeyk.sos;
 
-import org.springframework.test.context.ContextConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.transaction.BeforeTransaction;
 
 /**
  * @author Shengzhao Li
  */
-@ContextConfiguration(locations = {""}, initializers = {TestApplicationContextInitializer.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 public abstract class ContextTest extends AbstractTransactionalJUnit4SpringContextTests {
 
+
     @BeforeTransaction
-    public void beforeTest() {
+    public void before() throws Exception {
 
     }
+
+
 }
